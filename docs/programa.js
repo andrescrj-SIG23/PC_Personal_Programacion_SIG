30c720539d909837f3550ff43a9d6d6d86da62a7
andrescrj-SIG23 <andrescromeroj@gmail.com>
2025-08-05 14:20:31 -0500

    Version 0.3

--- docs/programa.js
+++ docs/programa.js
@@ -1,1 +1,5 @@
 var map = L.map('map').setView([4.628104870521137, -74.06590505390936], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
